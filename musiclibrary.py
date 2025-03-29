b98a658657bcf3f83805e66b9466f754cc89ade1
Amardeep1792 <amardeepkumaray@gmail.com>
2025-03-29 23:42:18 +0530

    added

---
+++ musiclibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "all the stars": "https://www.youtube.com/watch?v=JQbjS0_ZfJ0",
+    "suzume":"https://www.youtube.com/watch?v=qal34e9v_pk",
+    "never say never": "https://www.youtube.com/watch?v=_Z5-P9v3F8w"
+}
---
+++ others.py
@@ -0,0 +1,2 @@
+#news api
+newsapi = " https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY"
